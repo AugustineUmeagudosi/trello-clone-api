884ed9ab1cc5a7be4761ec3948244a1f260b1258
Austine <augustineumeagudosi@gmail.com>
2021-03-28 20:53:54 +0100

    still fixing bugs :(

--- seeders/20210325091827-organizationMemberRoles.js
+++ seeders/20210325091827-organizationMemberRoles.js
@@ -3,9 +3,9 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('OrganizationMembersRoles', [
-      { role: 'Member', createdAt: new Date(), uuid: uuidv4(), updatedAt: new Date() }, 
-      { role: 'Supervisor', createdAt: new Date(), uuid: uuidv4(), updatedAt: new Date() }, 
-      { role: 'Creator', createdAt: new Date(), uuid: uuidv4(), updatedAt: new Date() } 
+      { id: uuidv4(), role: 'Member', createdAt: new Date(), updatedAt: new Date() }, 
+      { id: uuidv4(), role: 'Supervisor', createdAt: new Date(), updatedAt: new Date() }, 
+      { id: uuidv4(), role: 'Creator', createdAt: new Date(), updatedAt: new Date() } 
     ]);
   },
   down: (queryInterface, Sequelize) => {
